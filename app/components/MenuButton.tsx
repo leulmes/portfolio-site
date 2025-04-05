9c39e0b123675f5bf22d8d93b362445d91b63df6
Leul Mesfin <leulmesfin152025@gmail.com>
2025-04-05 17:15:11 -0400

    finished mobile nav menu

---
+++ app/components/MenuButton.tsx
@@ -0,0 +1,81 @@
+"use client";
+import React, { useState } from "react";
+import { motion, MotionConfig } from "motion/react";
+
+const AnimatedBurger = () => {
+	const [active, setActive] = useState(false);
+
+	return (
+		<MotionConfig
+			transition={{
+				duration: 0.3,
+				ease: "easeInOut",
+			}}
+		>
+			<motion.button
+				initial={false}
+				onClick={() => setActive((pv) => !pv)}
+				className="relative h-20 w-20 rounded-full bg-white/0 transition-colors hover:bg-white"
+				animate={active ? "open" : "closed"}
+			>
+				<motion.span
+					style={{
+						left: "50%",
+						top: "35%",
+						x: "-50%",
+						y: "-50%",
+					}}
+					className="absolute h-1 w-10 bg-white rounded border"
+					variants={{
+						open: {
+							rotate: ["0deg", "0deg", "45deg"],
+							top: ["35%", "50%", "50%"],
+						},
+						closed: {
+							rotate: ["45deg", "0deg", "0deg"],
+							top: ["50%", "50%", "35%"],
+						},
+					}}
+				></motion.span>
+				<motion.span
+					style={{
+						left: "50%",
+						top: "50%",
+						x: "-50%",
+						y: "-50%",
+					}}
+					className="absolute h-1 w-10 bg-white rounded border"
+					variants={{
+						open: {
+							rotate: ["0deg", "0deg", "-45deg"],
+						},
+						closed: {
+							rotate: ["-45deg", "0deg", "0deg"],
+						},
+					}}
+				></motion.span>
+				<motion.span
+					style={{
+						left: "50%",
+						bottom: "35%",
+						x: "-50%",
+						y: "50%",
+					}}
+					className="absolute h-1 w-10 bg-white rounded border"
+					variants={{
+						open: {
+							rotate: ["0deg", "0deg", "-45deg"],
+							bottom: ["35%", "50%", "50%"],
+						},
+						closed: {
+							rotate: ["-45deg", "0deg", "0deg"],
+							bottom: ["50%", "50%", "35%"],
+						},
+					}}
+				></motion.span>
+			</motion.button>
+		</MotionConfig>
+	);
+};
+
+export default AnimatedBurger;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AnimatedBurger from "./MenuButton";
+
+interface NavLink {
+	title: string;
+	href: string;
+}
+// const navLinks: NavLink = [
+//     {title: 'Home', href: '/home'},
+//     {title: 'Experience', href: '/experience'},
+//     {title: 'Projects', href: '/projects'},
+//     {title: 'Contact', href: '/contact'},
+// ];
+
+const Navbar = () => {
+	return (
+		<div className="flex border-b-[0.1px] h-16 border-white justify-end items-center">
+			<AnimatedBurger />
+		</div>
+	);
+};
+
+export default Navbar;
