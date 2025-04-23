f3ac5aa3bd882e18fb2fc20cfbf65e61f7cb4f14
Leul Mesfin <leulmesfin152025@gmail.com>
2025-04-23 12:23:38 -0400

    updating some changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const updateDate = (): number => {
@@ -10,7 +11,16 @@ const Footer = () => {
 			<div className="flex flex-col items-center justify-center font-main text-lg mb-14">
 				<span>&copy; {updateDate()}</span>
 				<span className="flex gap-1">
-					Crafted by Leul Mesfin — powered by
+					Crafted by
+					<Link
+						href={"https://github.com/leulmes/portfolio-site"}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="cursor-pointer hover:underline underline-offset-4"
+					>
+						Leul Mesfin
+					</Link>{" "}
+					— powered by
 					<span className="animate-pulse">🍪</span>
 				</span>
 			</div>
